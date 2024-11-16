110d4a35f8dbdfe6cbdc487fa0f0f62ca124d7d9
Oussama BOUHLEL <oussama_bouhlel@outlook.fr>
2024-11-17 00:50:54 +0100

    Add Game Over Event

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ fn main() {
         .init_resource::<Score>()
         .init_resource::<StarSpawnTimer>()
         .init_resource::<EnemySpawnTimer>()
+        .add_event::<GameOver>()
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_player)
         .add_startup_system(spawn_enemies)
@@ -34,6 +35,7 @@ fn main() {
         .add_system(tick_enemy_spwan_timer)
         .add_system(spawn_enemy_over_time)
         .add_system(exit_game)
+        .add_system(handle_game_over)
         .run();
 }
 
@@ -83,6 +85,10 @@ impl Default for StarSpawnTimer {
             timer: Timer::from_seconds(STAR_SPAWN_TIME, TimerMode::Repeating),
         }
     }
+}
+
+pub struct GameOver {
+    pub score: u32,
 }
 
 pub fn spawn_player(
@@ -300,10 +306,12 @@ pub fn confine_enemy_mouvement(
 
 pub fn enemy_hit_player(
     mut commands: Commands,
+    mut game_over_event_writer: EventWriter<GameOver>,
     mut player_query: Query<(Entity, &Transform), With<Player>>,
     enemy_query: Query<&Transform, With<Enemy>>,
     asset_server: Res<AssetServer>,
     audio: Res<Audio>,
+    score: Res<Score>,
 ) {
     if let Ok((player_entity, player_transform)) = player_query.get_single_mut() {
         for enemy_transform in enemy_query.iter() {
@@ -318,6 +326,7 @@ pub fn enemy_hit_player(
                 let sound_effect = asset_server.load("audio/impactSoft_heavy_000.ogg");
                 audio.play(sound_effect);
                 commands.entity(player_entity).despawn();
+                game_over_event_writer.send(GameOver { score: score.value })
             }
         }
     }
@@ -414,5 +423,11 @@ pub fn exit_game(
 ) {
     if keyboard_input.just_pressed(KeyCode::Escape) {
         app_exit_event_writer.send(AppExit);
+    }
+}
+
+pub fn handle_game_over(mut game_over_event_reader: EventReader<GameOver>) {
+    for event in game_over_event_reader.iter() {
+        println!("Your final score is : {}", event.score.to_string());
     }
 }
